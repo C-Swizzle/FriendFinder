ccb03da86bb5a7d2bc9843f2a37762a9da96002e
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-03-23 12:41:35 -0700

    >

---
+++ server.js
@@ -0,0 +1,34 @@
+var express=require("express");
+var path=require("path");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"/app/public/home.html"));
+});
+app.get("/survey",function(req,res){
+    res.sendFile(path.join(__dirname,"/app/public/survey.html"));
+})
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
