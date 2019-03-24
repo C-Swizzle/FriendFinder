bece784c4995f1cfaf078a7d64df6c63799d3afe
Chris <chrisem816@gmail.com>
2019-03-23 20:20:41 -0700

    working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,7 @@
 var friends =[
-    {name: "cmack", picUrl: "https://github.com/C-Swizzle/giphy-api", questions: [1,1]}
+    { name: 'cmack',
+  picUrl: 'https://github.com/C-Swizzle/giphy-api',
+  questions: [ '1', '1', '1', '1', '1', '1', '1' ] },
+    {name:"neda",picUrl:"https://previews.123rf.com/images/tony4urban/tony4urban1702/tony4urban170200099/73165930-caffeine-chemical-formula-science-symbol-elements-reaction.jpg",questions:[ '1', '1', '1', '1', '1', '1', '1' ]}
 ];
 module.exports=friends;
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get("/survey",function(req,res){
 })
 app.post("/api/friends",function(req,res){
     console.log(req.body);
+    res.send(checkFriends(friends,req.body.questions));
     friends.push(req.body);
 });
 
@@ -29,13 +30,40 @@ app.get("/api/friends",function(req,res){
     res.json(friends);
 });
 
+function checkFriends(friendsArrOfObj,reqinputArr){
+    var tempArr=[];
+for (var i=0;i<friendsArrOfObj.length;i++){
+    var count=0;
+        for (var j=0;j<reqinputArr.length;j++){
+            count += Math.abs(Number(reqinputArr[j])-Number(friendsArrOfObj[i].questions[j]));
+        }
+        tempArr.push(count);
+}
+var thingToCheckNow=doBigThingsWithCallbacksAndSuchVeryExciting(sortArr,tempArr,friendsArrOfObj);
+return thingToCheckNow;
+};
 
-
-
-
+function sortArr(arr){
+    var tempArr=arr;
+    for (var i=0;i<arr.length;i++){
+        for(var j=0;j<arr.length;j++){
+            if(tempArr[j+1]<tempArr[j]){
+                var temp=tempArr[j];
+                tempArr[j]=tempArr[j+1];
+                tempArr[j+1]=temp;
+            }
+        }
+    }
+    return tempArr;
+};
+console.log(sortArr([1,67,8,9,4,5,6,88,7]));
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
-  
+function doBigThingsWithCallbacksAndSuchVeryExciting(sortFunc,Arr,friendsArrOfObj){
+    var newArr= sortFunc(Arr);
+    var index=Arr.indexOf(newArr[0]);
+    return friendsArrOfObj[index];
+}
