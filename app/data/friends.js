503b8402f276a2035402dfb3308353b2357df2e6
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-03-28 18:22:41 -0700

    done basicalllly

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends =[
     { name: 'cmack',
-  picUrl: 'https://github.com/C-Swizzle/giphy-api',
+  picUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Integral_as_region_under_curve.svg/800px-Integral_as_region_under_curve.svg.png',
   questions: [ '1', '1', '1', '1', '1', '1', '1' ] },
     {name:"neda",picUrl:"https://previews.123rf.com/images/tony4urban/tony4urban1702/tony4urban170200099/73165930-caffeine-chemical-formula-science-symbol-elements-reaction.jpg",questions:[ '1', '1', '1', '1', '1', '1', '1' ]}
 ];
