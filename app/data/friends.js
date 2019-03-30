8dc533d711061369d681da99150a81425d530bd1
Chris <chrisem816@gmail.com>
2019-03-29 20:36:37 -0700

    kmhm

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends =[
     { name: 'cmack',
-  picUrl: 'https://github.com/C-Swizzle/giphy-api',
+  picUrl: 'https://am14.akamaized.net/med/cnt/uploads/2014/06/pi.jpg',
   questions: [ '1', '1', '1', '1', '1', '1', '1' ] },
     {name:"neda",picUrl:"https://previews.123rf.com/images/tony4urban/tony4urban1702/tony4urban170200099/73165930-caffeine-chemical-formula-science-symbol-elements-reaction.jpg",questions:[ '1', '1', '1', '1', '1', '1', '1' ]}
 ];
